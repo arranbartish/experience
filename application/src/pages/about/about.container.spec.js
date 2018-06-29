03c473e61771b97b4565dd1208da1b6d01067030
Arran Bartish <abartish@teksystems.com>
2018-06-29 11:07:52 -0400

    fix scrol

--- application/src/pages/about/about.container.spec.js
+++ application/src/pages/about/about.container.spec.js
@@ -1,16 +1,16 @@
-import {HomeComponent} from './home.container';
+import {AboutComponent} from './about.container';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-describe('Home container', () => {
+describe('About container', () => {
 
-  const user = {};
-  const component = shallow(<HomeComponent user={user} />);
-  it('should be defined', () => {
-    expect(HomeComponent).toBeDefined();
+  const name = 'Arran';
+  const component = shallow(<AboutComponent user={name}/>);
+  it('will be defined', () => {
+    expect(AboutComponent).toBeDefined();
   });
 
-  it('should render', () => {
-    expect(component.find('.home').exists()).toBeTruthy();
+  it('will render', () => {
+    expect(component.find('.about').exists()).toBeTruthy();
   });
 });
--- application/src/root/root.routes.js
+++ application/src/root/root.routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import About from '../pages/about/about.container';
 import Main from '../templates/main/main.component';
 
 export default (
   <Route path="/" component={Main}>
+    <IndexRoute component={About}/>
     <Route path="about" component={About}/>
     <Route path="*" component={About}/>
   </Route>
