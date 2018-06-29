0b7b8bd6898071ba6660852307bfe0a0a5ca4a5f
Arran Bartish <abartish@teksystems.com>
2018-06-29 15:08:36 -0400

    move from the template into the page

--- application/src/pages/about/about.container.js
+++ application/src/pages/about/about.container.js
@@ -2,12 +2,15 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
+import {Portrait} from "../../components/portrait/portrait.component";
+
 import './about.scss';
 
 class About extends Component {
   render() {
     return (
       <div className="about">
+        <Portrait/>
         <h1>Hello {this.props.user.name}!</h1>
         <p className="lead">This is where Arran will describe himself.<br />He will go through some detail and that is ok.</p>
       </div>
