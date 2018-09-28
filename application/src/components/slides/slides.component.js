f3b2be141b3fc79e506f713b461d063f4ee3b664
Arran Bartish <arranbartish@hotmail.com>
2018-09-28 13:25:31 -0400

    add new slide component

---
+++ application/src/components/slides/slides.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './slides.scss';
+
+export const Slides = () => {
+
+  return (
+    <div className="slides">
+      <div className="col-xs-12">
+        <a href="https://docs.google.com/presentation/d/1sicG8fuzsY8eGsTItDLdDEPpv0-dx9-AIvavQ3Ne0T8/edit?usp=sharing">
+          <span className="slides__title">Approval testing with style: Catching unexpected changes to your application styles</span>
+        </a>
+      </div>
+    </div>
+  );
+};
